94225ae4da79f93036233aa4454c9c989d2cea61
Sanket Kumar Choudhary <sanketkumarchoudhary@gmail.com>
2022-05-19 07:49:35 +0530

    Added with Anime Downloader using Selenium
    Script that helps to download Anime using Selenium completely automated with user input

---
+++ animedownloader_selenium.py
@@ -0,0 +1,39 @@
+import os
+import time
+import pyautogui
+
+url=input("Video URL without episode: ")
+start = int(input("First Episode to Download: "))
+end = int(input("Last Episode to Download: "))+1
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+caps = DesiredCapabilities().CHROME
+# caps["pageLoadStrategy"] = "normal"  #  complete
+caps["pageLoadStrategy"] = "eager"  # interactive
+# caps["pageLoadStrategy"] = "none"   #  undefined
+chromedriver = r"C:\Users\USER\Downloads\Compressed\chromedriver.exe"
+
+os.environ["webdriver.chrome.driver"] = chromedriver
+chrome_options = Options()
+
+# this is the preference we're passing
+prefs = {'profile.default_content_setting_values.automatic_downloads': 1}
+chrome_options.add_experimental_option("prefs", prefs)
+chrome_options.add_argument("--start-maximized")
+driver = webdriver.Chrome(desired_capabilities=caps, chrome_options=chrome_options)
+
+
+for i in range(start, end):
+    driver.get(url + str(i))
+    driver.execute_script("window.scrollTo(0, 100)")
+    # a = pyautogui.locateOnScreen('img.png', grayscale=True)
+    driver.find_element('xpath',
+                        "/html/body/div[2]/div/div/section/section[1]/div[1]/div[2]/div[1]/div[5]/ul/li[1]/a").click()
+    time.sleep(5)
+    pyautogui.click('img_480.png')
+    time.sleep(1)
+    driver.switch_to.window(driver.window_handles[1])
+    driver.close()
+    driver.switch_to.window(driver.window_handles[0])
