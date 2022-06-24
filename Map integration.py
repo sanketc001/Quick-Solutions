23b80a6cb216932ad7ab71b7061db71993e1fe69
Sanket Kumar Choudhary <sanketkumarchoudhary@gmail.com>
2022-06-24 23:59:30 +0530

    Update Map integration.py

--- Map integration.py
+++ Map integration.py
@@ -1,5 +1,21 @@
 import pyautogui
+import csv
+import time
+rows = []
+with open("Maharashtra.csv", 'r') as file:
+    csvreader = csv.reader(file)
+    header = next(csvreader)
+    for row in csvreader:
+        rows.append(row)
+print(header)
+'''
+355,111
 
+'''
+time.sleep(5)
+pyautogui.mouseInfo()
+for i in rows:
+    print(i)
 # import gmaps
 # import gmaps.datasets
 #
