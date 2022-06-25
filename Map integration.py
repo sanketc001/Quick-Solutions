636984915bb52d2bea1128646be814184b5d0a4f
Sanket Kumar Choudhary <sanketkumarchoudhary@gmail.com>
2022-06-25 23:48:40 +0530

    updated Map integration

--- Map integration.py
+++ Map integration.py
@@ -10,12 +10,26 @@ with open("Maharashtra.csv", 'r') as file:
 print(header)
 '''
 355,111
-
+781,110
+675,405
+410,142
 '''
 time.sleep(5)
-pyautogui.mouseInfo()
+for j in range(20):
+    pyautogui.locate('mark.png')
+    pyautogui.scroll(100)
+    time.sleep(1)
+#pyautogui.mouseInfo()
 for i in rows:
     print(i)
+    pyautogui.click(355,111)
+    pyautogui.write(i[0])
+    pyautogui.click(781,110)
+    time.sleep(1)
+    pyautogui.click('cross.png')
+    for j in range(20):
+        pyautogui.locate('mark.png')
+        pyautogui.scroll(100)
 # import gmaps
 # import gmaps.datasets
 #
