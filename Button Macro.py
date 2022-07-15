b1f0c90c414df191224822338f26e13cdcab1589
Sanket Kumar Choudhary <sanketkumarchoudhary@gmail.com>
2022-07-15 23:25:36 +0530

    added button macro and ngrok script

---
+++ Button Macro.py
@@ -0,0 +1,6 @@
+import time
+import pyautogui
+time.sleep(10)
+while True:
+    pyautogui.write("consume water")
+    time.sleep(3)consume water
---
+++ ngrok.bat
@@ -0,0 +1,2 @@
+ngrok tcp -region in 25565
+echo pause
