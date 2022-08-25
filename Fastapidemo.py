b204f82a2b0dbadf3c4808dfcc17ff1cbbfba6b6
Sanket Kumar Choudhary <sanketkumarchoudhary@gmail.com>
2022-08-26 00:39:50 +0530

    Update Fastapidemo.py

--- Fastapidemo.py
+++ Fastapidemo.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 app = FastAPI()
 
-@app.get("/{item_id}")
-async def read_item(item_id):
-    return {"item_id": item_id}
+@app.get("/")
+def hello():
+
+  return {"Hello world!"}
