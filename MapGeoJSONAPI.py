d6327e9426acc2aed4ceffb36fd299270fd3552a
Sanket Kumar Choudhary <sanketkumarchoudhary@gmail.com>
2022-08-27 23:29:38 +0530

    Update MapGeoJSONAPI.py

--- MapGeoJSONAPI.py
+++ MapGeoJSONAPI.py
@@ -1,17 +1,24 @@
 #import geojson
 import json
-file=open("Beed-Khasara.geojson","r")
-l=[]
-for i in file:
-  try:
-    l.append(dict(json.loads(i[:-2])))
-  except:
-    pass
-print(l[0])
-filename = "Beed-Khasara.txt"
-with open(filename, 'w') as csvfile:
-  for i in l:
-    csvfile.writelines(str(i)+"\n")
-f=open("Beed-Khasara.txt","r")
-for i in f:
-  print(i)
+from fastapi import FastAPI
+import pickle
+app = FastAPI()
+# file=open("Beed-Khasara.geojson","r")
+# l=[]
+# for i in file:
+#   try:
+#     l.append(dict(json.loads(i[:-2])))
+#   except:
+#     pass
+# d={}
+# for i in l:
+#   d.update({i['properties']['Khasra_No']:i})
+# print(d)
+# filename = "Khasaradict.pkl"
+# geeky_file = open(filename, 'wb')
+# pickle.dump(d, geeky_file)
+# geeky_file.close()
+@app.get("/{k}")
+async def read_item(k):
+    f = open("Khasaradict.pkl", "rb")
+    return str(pickle.load(f)[k])
